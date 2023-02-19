a95c7bdbcad164ed7aa1118fdfa3d7d7fbabde78
Andrey Lechev <andrey@lechev.me>
2023-02-19 17:57:29 +0100

    Update TS types and interfaces

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,35 @@
+export interface CapacitorOCROptions {
+  base64Image: string;
+  orientation: 'up' | 'down' | 'left' | 'right';
+  languages?: string[];
+}
+
+export interface CapacitorOCRResult {
+  text: string;
+  lines: CapacitorOCRLine[];
+  confidence?: number;
+}
+
+export interface CapacitorOCRLine {
+  text: string;
+  bbox: CapacitorOCRBBox;
+  words?: CapacitorOCRWord[];
+  confidence?: number;
+}
+
+export interface CapacitorOCRWord {
+  text: string;
+  bbox: CapacitorOCRBBox;
+  confidence?: number;
+}
+
+export interface CapacitorOCRBBox {
+  x0: number;
+  y0: number;
+  x1: number;
+  y1: number;
+}
+
 export interface CapacitorOCRPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  recognize(options: CapacitorOCROptions): Promise<CapacitorOCRResult[]>;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,17 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { CapacitorOCRPlugin } from './definitions';
+import type {
+  CapacitorOCROptions,
+  CapacitorOCRPlugin,
+  CapacitorOCRResult,
+} from './definitions';
 
 export class CapacitorOCRWeb extends WebPlugin implements CapacitorOCRPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  async recognize(options: CapacitorOCROptions): Promise<CapacitorOCRResult[]> {
+    console.log(
+      'This plugin is not intended to work in the browser. Please use tesseract.js (https://tesseract.projectnaptha.com/) if you want the functionality.',
+      options,
+    );
+    return [];
   }
 }
