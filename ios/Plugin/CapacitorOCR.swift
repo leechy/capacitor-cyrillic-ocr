e3b87bd93fcc55aa5e773046847a17f3ef2a34fc
Andrey Lechev <andrey@lechev.me>
2023-03-14 10:54:28 +0100

    Move `recognize` method to the object file to make it look better

--- ios/Plugin/CapacitorOCR.swift
+++ ios/Plugin/CapacitorOCR.swift
@@ -1,8 +1,118 @@
 import Foundation
+import Capacitor
+import Vision
 
 @objc public class CapacitorOCR: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+
+    var recognizedText: [String: Any] = [
+      "text": "",
+      "lines": []
+    ]
+    var imageWidth: Double = 0
+    var imageHeight: Double = 0
+
+    @objc public func recognize(call: CAPPluginCall, languages: Array<String>, cgImage: CGImage, orientation: CGImagePropertyOrientation) {
+        // caching image size to convert the bboxes later
+        self.imageWidth = Double(cgImage.width)
+        self.imageHeight = Double(cgImage.height)
+
+        // create the request
+        let request = VNRecognizeTextRequest(completionHandler: self.handleDetectedText)
+        request.recognitionLevel = .accurate
+        request.recognitionLanguages = languages
+        
+        // create the rerquest handler
+        let imageRequestHandler = VNImageRequestHandler(
+          cgImage: cgImage,
+          orientation: orientation,
+          options: [:]
+        )
+        
+        // add to the global queue
+        DispatchQueue.global(qos: .userInitiated).async {
+          do {
+            try imageRequestHandler.perform([request])
+            call.resolve(self.recognizedText)
+          } catch let error as NSError {
+            print("Failed to perform image request: \(error)")
+            call.reject(error.description)
+          }
+        }
+    }
+
+    ///  Transforms the VNRequest to the JS Array with the recognized text
+    ///
+    ///  - Parameters:
+    ///    - request: pointer to the request to the Vision framework, to get the results
+    ///    - error: error message if something is wrong
+    ///
+    ///  - Returns:
+    func handleDetectedText(request: VNRequest?, error: Error?) {
+      if let error = error {
+        NSLog("Error detecting text: \(error)")
+        return
+      }
+      
+      guard let results = request?.results, results.count > 0 else {
+        NSLog("No text found")
+        return
+      }
+      
+      var fullText = ""
+      var lines: [[String: Any]] = []
+      for result in results {
+        if let observation = result as? VNRecognizedTextObservation {
+          for text in observation.topCandidates(1) {
+            let bbox = self.convertBbox(boundingBox: observation.boundingBox, width: self.imageWidth, height: self.imageHeight)
+            fullText += text.string + "\n"
+            lines.append([
+              "text": text.string,
+              "confidence": text.confidence,
+              "bbox": bbox,
+              "words": [
+                "text": text.string,
+                "confidence": text.confidence,
+                "bbox": bbox,
+              ]
+            ])
+          }
+        }
+      }
+      
+      // return the tesseract-like results
+      self.recognizedText = [
+        "text": fullText,
+        "lines": lines
+      ]
+    }
+
+    /// Convert Vision coordinates to pixel coordinates within image.
+    ///
+    /// - Parameters:
+    ///   - boundingBox: The bounding box returned by Vision framework.
+    ///   - width: Image width in pixels.
+    ///   - height: Image height in pixels.
+    ///
+    /// - Returns: The bounding box in pixel coordinates within the initial image.
+
+    func convertBbox(boundingBox: CGRect, width imageWidth: Double, height imageHeight: Double) -> [String : CGFloat] {
+      // Begin with input rect.
+      var rect = boundingBox
+
+      // Reposition origin.
+      rect.origin.x *= imageWidth
+      rect.origin.y = (1 - rect.maxY) * imageHeight
+
+      // Rescale normalized coordinates.
+      rect.size.width *= imageWidth
+      rect.size.height *= imageHeight
+    
+      let bbox = [
+        "x0": rect.origin.x,
+        "y0": rect.origin.y,
+        "x1": rect.origin.x + rect.size.width,
+        "y1": rect.origin.y + rect.size.height
+      ]
+      return bbox
     }
 }
--- ios/Plugin/CapacitorOCRPlugin.swift
+++ ios/Plugin/CapacitorOCRPlugin.swift
@@ -10,17 +10,9 @@ import Vision
 public class CapacitorOCRPlugin: CAPPlugin {
   private let implementation = CapacitorOCR()
 
-  var recognizedText: [String: Any] = [
-    "text": "",
-    "lines": []
-  ]
-  var languages = ["en"]
-  var imageWidth: Double = 0
-  var imageHeight: Double = 0
-  
   @objc func recognize(_ call: CAPPluginCall) {
-    self.languages = call.getArray("languages", ["en"]) as! Array<String>
-    
+    // process parameters
+    let languages = call.getArray("languages", ["en"]) as! Array<String>
     let orientation = self.getOrientation(orientation: call.getString("orientation"))
 
     // decode image
@@ -38,82 +30,10 @@ public class CapacitorOCRPlugin: CAPPlugin {
       return
     }
     
-    // caching image size to convert the bboxes later
-    self.imageWidth = Double(cgImage.width)
-    self.imageHeight = Double(cgImage.height)
-
-    // create the request
-    let request = VNRecognizeTextRequest(completionHandler: self.handleDetectedText)
-    request.recognitionLevel = .accurate
-    request.recognitionLanguages = languages
-    
-    // create the rerquest handler
-    let imageRequestHandler = VNImageRequestHandler(
-      cgImage: cgImage,
-      orientation: orientation,
-      options: [:]
-    )
-    
-    // add to the global queue
-    DispatchQueue.global(qos: .userInitiated).async {
-      do {
-        try imageRequestHandler.perform([request])
-        call.resolve(self.recognizedText)
-      } catch let error as NSError {
-        print("Failed to perform image request: \(error)")
-        call.reject(error.description)
-      }
-    }
-  }
-  
-  func handleDetectedText(request: VNRequest?, error: Error?) {
-    if let error = error {
-      NSLog("Error detecting text: \(error)")
-      return
-    }
-    
-    guard let results = request?.results, results.count > 0 else {
-      NSLog("No text found")
-      return
-    }
-    
-    var fullText = ""
-    var lines: [[String: Any]] = []
-    for result in results {
-      if let observation = result as? VNRecognizedTextObservation {
-        for text in observation.topCandidates(1) {
-          
-          let rect = self.convertBbox(boundingBox: observation.boundingBox, width: self.imageWidth, height: self.imageHeight)
-          let bbox = [
-            "x0": rect.origin.x,
-            "y0": rect.origin.y,
-            "x1": rect.origin.x + rect.size.width,
-            "y1": rect.origin.y + rect.size.height
-          ]
-          
-          fullText += text.string + "\n"
-          lines.append([
-            "text": text.string,
-            "confidence": text.confidence,
-            "bbox": bbox,
-            "words": [
-              "text": text.string,
-              "confidence": text.confidence,
-              "bbox": bbox,
-            ]
-          ])
-        }
-      }
-    }
-    
-    // return the tesseract-like results
-    self.recognizedText = [
-      "text": fullText,
-      "lines": lines
-    ]
+    implementation.recognize(call: call, languages: languages, cgImage: cgImage, orientation: orientation) 
   }
 
-  
+
   /// Converts the orientation from string to CGImagePropertyOrientation value
   ///
   /// Defaults to "up"
@@ -131,31 +51,5 @@ public class CapacitorOCRPlugin: CAPPlugin {
       default: return CGImagePropertyOrientation.up
     }
   }
-  
-  /// Convert Vision coordinates to pixel coordinates within image.
-  ///
-  /// Adapted from `boundingBox` method from
-  /// [Detecting Objects in Still Images](https://developer.apple.com/documentation/vision/detecting_objects_in_still_images).
-  /// This flips the y-axis.
-  ///
-  /// - Parameters:
-  ///   - boundingBox: The bounding box returned by Vision framework.
-  ///   - bounds: The bounds within the image (in pixels, not points).
-  ///
-  /// - Returns: The bounding box in pixel coordinates, flipped vertically so 0,0 is in the upper left corner
 
-  func convertBbox(boundingBox: CGRect, width imageWidth: Double, height imageHeight: Double) -> CGRect {
-    // Begin with input rect.
-    var rect = boundingBox
-
-    // Reposition origin.
-    rect.origin.x *= imageWidth
-    rect.origin.y = (1 - rect.maxY) * imageHeight
-
-    // Rescale normalized coordinates.
-    rect.size.width *= imageWidth
-    rect.size.height *= imageHeight
-  
-    return rect
-  }
 }
